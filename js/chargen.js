693433a9afac1b747fba57e9a1588cf4d0609124
Dainyl Cua <dainylcua@gmail.com>
2021-09-20 09:59:52 -0500

    CSS Transition Addition

--- js/chargen.js
+++ js/chargen.js
@@ -1,3 +1,7 @@
-$('#preset').on('click', () => {
-    alert("Not currently implemented.")
+// Simple CSS Fade Transition
+$('#transition').on('click', function() {
+    $('#tranScreen').fadeIn().fadeOut(2000)
+    $(this).parent().parent().fadeOut().remove(1000)
+    $(this).parent().parent().next().fadeIn(4000)
 })
+
