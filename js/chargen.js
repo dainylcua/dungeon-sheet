dfb9cb3376dcf8d0a6549be913d9cd453b456f03
Dainyl Cua <dainylcua@gmail.com>
2021-09-22 12:58:01 -0500

    Button styles and file beautify

--- js/chargen.js
+++ js/chargen.js
@@ -2,7 +2,7 @@
 BASEURL = 'https://www.dnd5eapi.co/api/'
 
 // Simple CSS Fade Transition
-$('.transition').on('click', function() {
+$('.transition').on('click', function () {
     $(this).parent().parent().fadeOut(500).remove(5)
     $('#tranScreen').fadeIn().fadeOut(1000, () => {
         $(this).parent().parent().next().fadeIn(300)
@@ -53,7 +53,7 @@ function populateRaceList() {
 
 // Adds list of races to dropdown menu
 function raceAddList() {
-    raceData.results.forEach(function(race) {
+    raceData.results.forEach(function (race) {
         $raceList.append(`<option value=${race.index}>${race.name}</option>`)
     })
 }
@@ -76,13 +76,13 @@ function populateRaceDetails() {
 
 // Manipulates data for selected race and renders
 function raceRender() {
-    $raceName.text(raceSelectData.name).css('font-size','35px')
+    $raceName.text(raceSelectData.name).css('font-size', '35px')
     $raceSpeed.text(`Speed: ${raceSelectData.speed} ft/6 seconds`)
     $raceSize.text(`Size: ${raceSelectData.size}-sized creature`)
 
     $raceBonusStats.text('Saving throw proficiencies: ')
     raceSelectData.ability_bonuses.forEach((ability) => {
-        if (raceSelectData.ability_bonuses.indexOf(ability) === raceSelectData.ability_bonuses.length-1) {
+        if (raceSelectData.ability_bonuses.indexOf(ability) === raceSelectData.ability_bonuses.length - 1) {
             $raceBonusStats.append(`<span class="bonusStat" id=${ability.ability_score.index}>${ability.ability_score.name} + ${ability.bonus}</span>`)
         } else {
             $raceBonusStats.append(`<span class="bonusStat" id=${ability.ability_score.index}>${ability.ability_score.name} + ${ability.bonus}, </span>`)
@@ -90,10 +90,10 @@ function raceRender() {
     })
 
     $raceProficiencies.text('')
-    if(raceSelectData.starting_proficiencies.length !== 0) {
+    if (raceSelectData.starting_proficiencies.length !== 0) {
         $raceProficiencies.text('Equipment and tool proficiencies: ')
         raceSelectData.starting_proficiencies.forEach((proficiency) => {
-            if(raceSelectData.starting_proficiencies.indexOf(proficiency) === raceSelectData.starting_proficiencies.length-1) {
+            if (raceSelectData.starting_proficiencies.indexOf(proficiency) === raceSelectData.starting_proficiencies.length - 1) {
                 $raceProficiencies.append(`<span class="proficiencies" id=${proficiency.index}>${proficiency.name}</span>`)
             } else {
                 $raceProficiencies.append(`<span class="proficiencies" id=${proficiency.index}>${proficiency.name}, </span>`)
@@ -103,37 +103,37 @@ function raceRender() {
 
 
     $raceProfOptions.text('')
-    if(raceSelectData.starting_proficiency_options !== undefined) {
-        $raceProfOptions.text(`Choose from ${raceSelectData.starting_proficiency_options.choose} of the following:`)
+    if (raceSelectData.starting_proficiency_options !== undefined) {
+        $raceProfOptions.text(`Choose from ${raceSelectData.starting_proficiency_options.choose} of the following proficiencies:`)
         $raceProfOptions.append('<br>')
         raceSelectData.starting_proficiency_options.from.forEach((proficiency) => {
-            $raceProfOptions.append(`<button class="chosen-proficiencies btn btn-primary m-1" id=${proficiency.index}>${proficiency.name}</button>`)
+            $raceProfOptions.append(`<button class="chosen-proficiencies btn btn-outline-dark m-1" data-bs-toggle="button" id=${proficiency.index}>${proficiency.name}</button>`)
         })
     }
 
     $raceLanguages.text(`Languages known: `)
     raceSelectData.languages.forEach((language) => {
-        if(raceSelectData.languages.indexOf(language) === raceSelectData.languages.length-1) {
-            $raceLanguages.append(`<span class="languages" id=${language.index}>${language.name}</span>`)    
+        if (raceSelectData.languages.indexOf(language) === raceSelectData.languages.length - 1) {
+            $raceLanguages.append(`<span class="languages" id=${language.index}>${language.name}</span>`)
         } else {
             $raceLanguages.append(`<span class="languages" id=${language.index}>${language.name}, </span>`)
         }
     })
 
     $raceLangOptions.text('')
-    if(raceSelectData.language_options !== undefined) {
+    if (raceSelectData.language_options !== undefined) {
         $raceLangOptions.text(`Choose from ${raceSelectData.language_options.choose} of the following:`)
         $raceLangOptions.append('<br>')
         raceSelectData.language_options.from.forEach((language) => {
-            $raceLangOptions.append(`<button class="chosen-languages btn btn-primary m-1" id=${language.index}>${language.name}</button>`)
+            $raceLangOptions.append(`<button class="chosen-languages btn btn-outline-dark m-1" data-bs-toggle="button" id=${language.index}>${language.name}</button>`)
         })
     }
 
     $raceTraits.text('')
-    if(raceSelectData.traits.length !== 0) {
+    if (raceSelectData.traits.length !== 0) {
         $raceTraits.text(`Racial Traits: `)
         raceSelectData.traits.forEach((trait) => {
-            if(raceSelectData.traits.indexOf(trait) === raceSelectData.traits.length-1) {
+            if (raceSelectData.traits.indexOf(trait) === raceSelectData.traits.length - 1) {
                 $raceTraits.append(`<span class="traits" id=${trait.index}>${trait.name}</span>`)
             } else {
                 $raceTraits.append(`<span class="traits" id=${trait.index}>${trait.name}, </span>`)
@@ -180,7 +180,7 @@ function populateClassList() {
 }
 
 function classAddList() {
-    classData.results.forEach(function(cls) {
+    classData.results.forEach(function (cls) {
         $classList.append(`<option value=${cls.index}>${cls.name}</option>`)
     })
 }
@@ -261,7 +261,7 @@ function classRender() {
 
     $classSavingThrows.text('Saving throw proficiencies: ')
     classSelectData.saving_throws.forEach((save) => {
-        if (classSelectData.saving_throws.indexOf(save) === classSelectData.saving_throws.length-1) {
+        if (classSelectData.saving_throws.indexOf(save) === classSelectData.saving_throws.length - 1) {
             $classSavingThrows.append(`<span class="saves" id=${save.index}>${save.name}</span>`)
         } else {
             $classSavingThrows.append(`<span class="saves" id=${save.index}>${save.name}, </span>`)
@@ -271,7 +271,7 @@ function classRender() {
 
     $classProficiencies.text('Equipment and tool proficiencies: ')
     classSelectData.proficiencies.forEach((prof) => {
-        if (classSelectData.proficiencies.indexOf(prof) === classSelectData.proficiencies.length -1) {
+        if (classSelectData.proficiencies.indexOf(prof) === classSelectData.proficiencies.length - 1) {
             $classProficiencies.append(`<span class="profs" id=${prof.index}>${prof.name}</span>`)
         } else {
             $classProficiencies.append(`<span class="profs" id=${prof.index}>${prof.name}, </span>`)
@@ -280,11 +280,11 @@ function classRender() {
     $classProficiencies.splice(0, -3)
 
     $classProfOptions.text('Skill proficiencies: ')
-    if(classSelectData.hasOwnProperty("proficiency_choices")) {
+    if (classSelectData.hasOwnProperty("proficiency_choices")) {
         classSelectData.proficiency_choices.forEach((choice) => {
             $classProfOptions.append(`<div class="prof-options choose${choice.choose}">Choose from ${choice.choose} of the following:</div>`)
             choice.from.forEach((prof) => {
-                $classProfOptions.append(`<button class="chosen-profs btn btn-primary m-1" id=${prof.index}>
+                $classProfOptions.append(`<button class="chosen-profs btn btn-outline-dark m-1" data-bs-toggle="button" id=${prof.index}>
                 ${prof.name}</button>`)
             })
             $classProfOptions.append('<br>')
@@ -296,7 +296,7 @@ function classRender() {
         $classEquipment.append(`<div class="equips" id=${equip.equipment.index}>${equip.equipment.name}
         x${equip.quantity}</div>`)
     })
-    
+
     $classEquipChoices.text('')
     classSelectData.starting_equipment_options.forEach((option) => {
         // For every equipment group option, choose an amount from multiple groups
@@ -309,21 +309,21 @@ function classRender() {
             // console.log('equip group', equipGroup)
             // Convert equip group object names into a completely new array
 
-            if(equipGroup.hasOwnProperty('0')) {
+            if (equipGroup.hasOwnProperty('0')) {
                 // If equip group has an object which is an object-array
                 eqObj = equipGroup
-                fragEquip = '<button class="equips btn btn-primary m-1">'
+                fragEquip = '<button class="equips btn btn-outline-dark m-1" data-bs-toggle="button">'
                 fragId = ''
                 // console.log('equip group', eqObj)
                 // Loop over the entire object and perform the same checks
                 Object.keys(eqObj).forEach((key) => {
                     eqObjOpt = eqObj[key]
                     // console.log('equip title', eqObjOpt)
-                    if(eqObjOpt.hasOwnProperty('equipment')) {
+                    if (eqObjOpt.hasOwnProperty('equipment')) {
                         // If equip group is a single item
                         fragEquip += `${eqObjOpt.equipment.name} x${eqObjOpt.quantity} `
                         fragId += `${eqObjOpt.equipment.index}-${eqObjOpt.quantity}-`
-                    } else if(eqObjOpt.hasOwnProperty('equipment_option')) {
+                    } else if (eqObjOpt.hasOwnProperty('equipment_option')) {
                         // If equip group has options
                         fragEquip += `${eqObjOpt.equipment_option.from.equipment_category.name} 
                         x${eqObjOpt.equipment_option.choose} `
@@ -336,18 +336,16 @@ function classRender() {
                 fragId = fragId.slice(0, -1)
                 // console.log(fragEquip, fragId)
                 $classEquipChoices.append(fragEquip)
-                
-            } else if(equipGroup.hasOwnProperty('equipment')) {
+
+            } else if (equipGroup.hasOwnProperty('equipment')) {
                 // If equip group is a single item
                 // console.log('hasequip')
-                $classEquipChoices.append(`<button class="equips btn btn-primary m-1" 
-                id="${equipGroup.equipment.index}-${equipGroup.quantity}">
-                ${equipGroup.equipment.name} x${equipGroup.quantity}`)
+                $classEquipChoices.append(`<button class="equips btn btn-outline-dark m-1" data-bs-toggle="button" id="${equipGroup.equipment.index}-${equipGroup.quantity}">${equipGroup.equipment.name} x${equipGroup.quantity}`)
 
-            } else if(equipGroup.hasOwnProperty('equipment_option')) {
+            } else if (equipGroup.hasOwnProperty('equipment_option')) {
                 // If equip group has options -- solely for choosing multiple martial/simple weapons
                 // console.log('hasequipoption')
-                $classEquipChoices.append(`<button class="equips btn btn-primary m-1" 
+                $classEquipChoices.append(`<button class="equips btn btn-outline-dark m-1" 
                 id="${equipGroup.equipment_option.from.equipment_category.index}-${equipGroup.equipment_option.choose}">
                 ${equipGroup.equipment_option.from.equipment_category.name} x${equipGroup.equipment_option.choose} </button>`)
             }
@@ -363,7 +361,7 @@ $('#roll-btn').on('click', rollStats)
 $('.increment').on('click', increaseStat)
 $('.decrement').on('click', decreaseStat)
 
-$('.transition-stat').on('click', function() {
+$('.transition-stat').on('click', function () {
     $(this).parent().parent().parent().fadeOut(500).remove(5)
     $('#tranScreen').fadeIn().fadeOut(1000, () => {
         $(this).parent().parent().parent().next().fadeIn(300)
@@ -371,14 +369,15 @@ $('.transition-stat').on('click', function() {
 })
 
 
-let currentStat, currentArray = [], statNum
+let currentStat, currentArray = [],
+    statNum
 
 let remainingPoints = parseInt($('#points-remaining').text())
 const $statPoints = $('#points-remaining')
 
 const statNames = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']
 
-function showRoll () {
+function showRoll() {
     $(this).parent().parent().parent().fadeOut(500)
     $(this).parent().parent().fadeOut(500).remove(5)
     $('#tranScreen').fadeIn().fadeOut(1000, () => {
@@ -387,7 +386,7 @@ function showRoll () {
     })
 }
 
-function showBuy () {
+function showBuy() {
     $(this).parent().parent().parent().fadeOut(500)
     $(this).parent().parent().fadeOut(500).remove(5)
     $('#tranScreen').fadeIn().fadeOut(1000, () => {
@@ -416,13 +415,13 @@ function increaseStat() {
     let decSib = $(this).parent().children('.decrement')
 
     // Exits loop if no points
-    if(remainingPoints === 0) {
+    if (remainingPoints === 0) {
         return
     }
 
     // Adds stats, decreases remaining points
-    if(statNum < 13) {
-        if(statNum === 8 && decSib.hasClass('btn-outline-danger')) {
+    if (statNum < 13) {
+        if (statNum === 8 && decSib.hasClass('btn-outline-danger')) {
             decSib.addClass('btn-outline-secondary').removeClass('btn-outline-danger')
             decSib.removeAttr('data-bs-toggle title')
         }
@@ -430,29 +429,29 @@ function increaseStat() {
         remainingPoints -= 1
         $statTarget.text(statNum)
         $statPoints.text(remainingPoints)
-    } else if(statNum < 15) {
-        statNum +=1
+    } else if (statNum < 15) {
+        statNum += 1
         remainingPoints -= 2
         $statTarget.text(statNum)
         $statPoints.text(remainingPoints)
     }
 
     // If stat is 15 after adding, cannot go higher
-    if(statNum === 15) {
-        $(this).attr('data-bs-toggle',"true")
-        $(this).attr('title',"Cannot go higher than 15")
+    if (statNum === 15) {
+        $(this).attr('data-bs-toggle', "true")
+        $(this).attr('title', "Cannot go higher than 15")
         $(this).removeClass('btn-outline-secondary').addClass('btn-outline-danger')
     }
 
     // If no points left, alerts user that they need points
-    if(remainingPoints === 0) {
-        $('.increment').attr('data-bs-toggle',"true")
-        $('.increment').attr('title',"No points remaining!")
+    if (remainingPoints === 0) {
+        $('.increment').attr('data-bs-toggle', "true")
+        $('.increment').attr('title', "No points remaining!")
         $('.increment').removeClass('btn-outline-secondary').addClass('btn-outline-danger')
         return
     }
-    
-    
+
+
 }
 
 function decreaseStat() {
@@ -461,15 +460,15 @@ function decreaseStat() {
     let incSib = $(this).parent().children('.increment')
 
     // If regaining points, remove danger from all except if stat = 15
-    if(remainingPoints === 0) {
+    if (remainingPoints === 0) {
         $('.increment').removeClass('btn-outline-danger').addClass('btn-outline-secondary')
         $('.increment').removeAttr('data-bs-toggle title')
         pointAlert()
     }
 
     // Remove stats, increases remaining points
-    if(statNum > 13) {
-        if(statNum === 15 && incSib.hasClass('btn-outline-danger')) {
+    if (statNum > 13) {
+        if (statNum === 15 && incSib.hasClass('btn-outline-danger')) {
             incSib.addClass('btn-outline-secondary').removeClass('btn-outline-danger')
             incSib.removeAttr('data-bs-toggle title')
         }
@@ -477,18 +476,18 @@ function decreaseStat() {
         remainingPoints += 2
         $statTarget.text(statNum)
         $statPoints.text(remainingPoints)
-    } else if(statNum > 8) {
-        if(statNum === 9) {
-            $(this).attr('data-bs-toggle',"true")
-            $(this).attr('title',"Cannot go lower than 8")
+    } else if (statNum > 8) {
+        if (statNum === 9) {
+            $(this).attr('data-bs-toggle', "true")
+            $(this).attr('title', "Cannot go lower than 8")
             $(this).removeClass('btn-outline-secondary').addClass('btn-outline-danger')
         }
-        statNum -=1
+        statNum -= 1
         remainingPoints += 1
         $statTarget.text(statNum)
         $statPoints.text(remainingPoints)
     }
-    
+
 }
 
 function pointAlert() {
@@ -497,8 +496,8 @@ function pointAlert() {
     for (i = 0; i < 5; i++) {
         if (parseInt($($eachStat[i]).text()) === 15) {
             $($eachIncrement[i]).removeClass('btn-outline-secondary').addClass('btn-outline-danger')
-            $($eachIncrement[i]).attr('data-bs-toggle',"true")
-            $($eachIncrement[i]).attr('title',"Cannot go higher than 15")
+            $($eachIncrement[i]).attr('data-bs-toggle', "true")
+            $($eachIncrement[i]).attr('title', "Cannot go higher than 15")
         }
     }
 }
